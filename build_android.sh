3a20e619628ec450aa3e7db935a3b33b376f607d
likun <likun@okay.cn>
2022-04-26 14:44:18 +0800

    文件夹重命名

--- build_android.sh
+++ build_android.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
  
 #你的NDK路径
-NDK=/Users/glumes/Downloads/android-ndk-r20b
+NDK=/Users/kunli/Library/Android/sdk/ndk/20.1.5948944
 TOOLCHAIN=$NDK/toolchains/llvm/prebuilt/darwin-x86_64
 API=21
  
@@ -10,6 +10,7 @@ echo "进入FFmpeg编译脚本"
 function build_android
 {
 echo "Compiling FFmpeg for $CPU"
+ echo $PREFIX
 
 ./configure \
     --prefix=$PREFIX \
@@ -39,46 +40,48 @@ echo "The Compilation of FFmpeg for $CPU is completed"
  
 #################### armv7-a ################################
  
-echo "开始编译FFmpeg(armeabi-v7a)"
-source "config-env.sh"
-
-cd ffmpeg
-
-OUTPUT_FOLDER="armeabi-v7a"
-ARCH="arm"
-CPU="armv7-a"
-TOOL_CPU_NAME=armv7a
-TOOL_PREFIX="$TOOLCHAIN/bin/arm-linux-androideabi"
- 
-CC="$TOOLCHAIN/bin/armv7a-linux-androideabi$API-clang"
-CXX="$TOOLCHAIN/bin/armv7a-linux-androideabi$API-clang++"
-SYSROOT="$NDK/toolchains/llvm/prebuilt/darwin-x86_64/sysroot"
-PREFIX="${PWD}/../ffmpeg_library/android/$OUTPUT_FOLDER"
-LIB_DIR="${PWD}/../ffmpeg_library/android/libs/$OUTPUT_FOLDER"
-OPTIMIZE_CFLAGS="-march=$CPU"
-build_android
- 
+#echo "开始编译FFmpeg(armeabi-v7a)"
+#source "config-env.sh"
+#
+#cd ffmpeg
+#
+#OUTPUT_FOLDER="armeabi-v7a"
+#ARCH="arm"
+#CPU="armv7-a"
+#TOOL_CPU_NAME=armv7a
+#TOOL_PREFIX="$TOOLCHAIN/bin/arm-linux-androideabi"
+#
+#CC="$TOOLCHAIN/bin/armv7a-linux-androideabi$API-clang"
+#CXX="$TOOLCHAIN/bin/armv7a-linux-androideabi$API-clang++"
+#SYSROOT="$NDK/toolchains/llvm/prebuilt/darwin-x86_64/sysroot"
+#PREFIX="${PWD}/../ffmpeg_library/android/$OUTPUT_FOLDER"
+#LIB_DIR="${PWD}/../ffmpeg_library/android/libs/$OUTPUT_FOLDER"
+#OPTIMIZE_CFLAGS="-march=$CPU"
+#build_android
+#
  
 #################### armv8-a ################################
  
-# echo "开始编译FFmpeg(arm64-v8a)"
-# source "config-env.sh"
-# OUTPUT_FOLDER="arm64-v8a"
-# ARCH=arm64
-# CPU="armv8-a"
-# TOOL_CPU_NAME=aarch64
-# TOOL_PREFIX="$TOOLCHAIN/bin/$TOOL_CPU_NAME-linux-android"
- 
-# CC="$TOOL_PREFIX$API-clang"
-# CXX="$TOOL_PREFIX$API-clang++"
-# SYSROOT="$NDK/toolchains/llvm/prebuilt/darwin-x86_64/sysroot"
-# PREFIX="${PWD}/android/$OUTPUT_FOLDER"
-# LIB_DIR="${PWD}/android/libs/$OUTPUT_FOLDER"
-# OPTIMIZE_CFLAGS="-march=$CPU"
-# build_android
+ echo "开始编译FFmpeg(arm64-v8a)"
+ source "config-env.sh"
+
+cd ffmpeg
+
+ OUTPUT_FOLDER="arm64-v8a"
+ ARCH=arm64
+ CPU="armv8-a"
+ TOOL_CPU_NAME=aarch64
+ TOOL_PREFIX="$TOOLCHAIN/bin/$TOOL_CPU_NAME-linux-android"
+
+ CC="$TOOL_PREFIX$API-clang"
+ CXX="$TOOL_PREFIX$API-clang++"
+ SYSROOT="$NDK/toolchains/llvm/prebuilt/darwin-x86_64/sysroot"
+ PREFIX="${PWD}/../ffmpeg_library/android/$OUTPUT_FOLDER"
+ LIB_DIR="${PWD}/../ffmpeg_library/android/libs/$OUTPUT_FOLDER"
+ OPTIMIZE_CFLAGS="-march=$CPU"
+ build_android
  
 #################### x86 ################################
- 
 # echo "开始编译FFmpeg(x86)"
 # source "config-env.sh"
 # OUTPUT_FOLDER="x86"
@@ -90,8 +93,8 @@ build_android
 # CC="$TOOL_PREFIX$API-clang"
 # CXX="$TOOL_PREFIX$API-clang++"
 # SYSROOT="$NDK/toolchains/llvm/prebuilt/darwin-x86_64/sysroot"
-# PREFIX="${PWD}/android/$OUTPUT_FOLDER"
-# LIB_DIR="${PWD}/android/libs/$OUTPUT_FOLDER"
+# PREFIX="${PWD}/../ffmpeg_library/android/$OUTPUT_FOLDER"
+# LIB_DIR="${PWD}/../ffmpeg_library/android/libs/$OUTPUT_FOLDER"
 # OPTIMIZE_CFLAGS="-march=i686 -mtune=intel -mssse3 -mfpmath=sse -m32"
 # build_android
  
@@ -108,7 +111,7 @@ build_android
 # CC="$TOOL_PREFIX$API-clang"
 # CXX="$TOOL_PREFIX$API-clang++"
 # SYSROOT="$NDK/toolchains/llvm/prebuilt/darwin-x86_64/sysroot"
-# PREFIX="${PWD}/android/$OUTPUT_FOLDER"
-# LIB_DIR="${PWD}/android/libs/$OUTPUT_FOLDER"
+# PREFIX="${PWD}/../ffmpeg_library/android/$OUTPUT_FOLDER"
+# LIB_DIR="${PWD}/../ffmpeg_library/android/libs/$OUTPUT_FOLDER"
 # OPTIMIZE_CFLAGS="-march=$CPU"
 # build_android
---
+++ ffmpeg_android_app/app/src/main/java/com/glumes/demo/MainActivity.java
@@ -0,0 +1,63 @@
+package com.glumes.demo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.Manifest;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import com.tbruyelle.rxpermissions2.RxPermissions;
+
+import java.io.File;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    static {
+        System.loadLibrary("native-lib");
+    }
+
+    private RxPermissions rxPermissions;
+
+    private String path = Environment.getExternalStorageDirectory() + File.separator + "resource/sintel.mp4";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Example of a call to a native method
+        Button tv = findViewById(R.id.sample_text);
+        tv.setText("Hello FFmpeg");
+
+        rxPermissions = new RxPermissions(this);
+        tv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                rxPermissions.request(
+                        Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                        Manifest.permission.READ_EXTERNAL_STORAGE
+                ).subscribe();
+                decodeWithPath(path);
+            }
+        });
+
+        findViewById(R.id.decode_surface).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this,DecodeSurfaceActivity.class));
+            }
+        });
+    }
+
+    /**
+     * A native method that is implemented by the 'native-lib' native library,
+     * which is packaged with this application.
+     */
+
+    public native void decodeWithPath(String path);
+}
